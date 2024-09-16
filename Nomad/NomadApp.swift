f42e1b17e63175cbeac8b72be89d7aaac745d316
nac5504 <nac5504@gmail.com>
2024-09-15 21:05:54 -0400

    Creating basic project

---
+++ Nomad/NomadApp.swift
@@ -0,0 +1,18 @@
+//
+//  NomadApp.swift
+//  Nomad
+//
+//  Created by Nicholas Candello on 9/15/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NomadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
