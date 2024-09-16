201dd883ad692f9cac7c75eb989ee731cb0383cc
jifonthespoon <austin.huguenard@gmail.com>
2024-09-16 12:17:36 -0400

    Added button

--- Nomad/Views/ItineraryPlanningView.swift
+++ Nomad/Views/ItineraryPlanningView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ItineraryPlanningView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Start Planning") {
+            print("Let's Go!")
+        }
     }
 }
 
