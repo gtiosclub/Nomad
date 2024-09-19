12b52ae7671ad4711c3e7e653dd84ad3a1ec4c22
shaunakkarnik <shaunak.karnik.14@gmail.com>
2024-09-19 19:02:58 -0400

    changed tezt

---
+++ Nomad/Views/DetailRecapView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailRecapView.swift
+//  Nomad
+//
+//  Created by Shaunak Karnik on 9/19/24.
+//
+
+import SwiftUI
+
+struct DetailRecapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailRecapView()
+}
