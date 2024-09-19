b3c4c9e0ba924f788e31a06c657069d00d997511
Amber Verma <ambersverma@gmail.com>
2024-09-19 17:44:11 -0400

    Added getter and setter functions for Hotel

--- Nomad/Models/Hotel.swift
+++ Nomad/Models/Hotel.swift
@@ -10,13 +10,14 @@ struct Hotel: POI {
     var name: String
     var rating: Double?
     var website: String?
-    
-    init(address: String, name: String, rating: Double? = nil) {
+
+    init(address: String, name: String, rating: Double? = nil, website: String? = nil) {
         self.address = address
         self.name = name
         self.rating = rating
+        self.website = website
     }
-    
+
     mutating func setAddress(newAddress: String) {
         self.address = newAddress
     }
@@ -24,4 +25,29 @@ struct Hotel: POI {
     mutating func setName(newName: String) {
         self.name = newName
     }
+    
+    mutating func setRating(newRating: Double?) {
+        self.rating = newRating
+    }
+    
+    mutating func setWebsite(newWebsite: String?) {
+        self.website = newWebsite
+    }
+
+    func getAddress() -> String {
+        return self.address
+    }
+    
+    func getName() -> String {
+        return self.name
+    }
+    
+    func getRating() -> Double? {
+        return self.rating
+    }
+    
+    func getWebsite() -> String? {
+        return self.website
+    }
 }
+
