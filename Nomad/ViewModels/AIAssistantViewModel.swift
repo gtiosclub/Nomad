cd2fc5f03b52a6c707c8666570f5b86e31f7ea2e
royankit11 <royankit11@gmail.com>
2024-09-19 18:18:35 -0400

    Testing

--- Nomad/ViewModels/AIAssistantViewModel.swift
+++ Nomad/ViewModels/AIAssistantViewModel.swift
@@ -15,7 +15,7 @@ class AIAssistantViewModel: ObservableObject {
         let url = URL(string: "https://api.yelp.com/v3/businesses/search")!
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
-        request.timeoutInterval = 10
+        request.timeoutInterval = 11
         request.allHTTPHeaderFields = [
           "accept": "application/json",
           "Authorization": "hpQdyXearQyP-ahpSeW2wDZvn-ljfmsGvN6RTKqo18I6R23ZB3dfbzAnEjvS8tWoPwyH9FFTGifdZ-n_qH80jbRuDbGb0dHu1qEPrLH-vqNq_d6TZdUaC_kZpwvqZnYx"
