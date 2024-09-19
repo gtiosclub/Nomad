f440e097566142760738d36154f27c97447b9bc0
Neel <neel468167@gmail.com>
2024-09-19 19:32:39 -0400

    edits to UserViewModel

--- Nomad/Models/Trip.swift
+++ Nomad/Models/Trip.swift
@@ -17,12 +17,15 @@ struct Trip {
     var created_date: String
     var modified_date: String
     
-    init(stops: [POI], start_location: POI, end_location: POI, start_date: String? = nil, end_date: String? = nil) {
-        self.stops = stops
+    init(start_location: POI, end_location: POI, start_date: String? = nil, end_date: String? = nil, stops: [POI]? = []) {
+        self.stops = stops ?? []
         self.start_location = start_location
         self.end_location = end_location
         self.start_date = start_date
         self.end_date = end_date
+        self.created_date = ""
+        self.modified_date = ""
+        self.id = UUID().uuidString
     }
     
     
--- Nomad/ViewModels/UserViewModel.swift
+++ Nomad/ViewModels/UserViewModel.swift
@@ -7,9 +7,20 @@
 
 import Foundation
 import MapKit
-
+// start loc, end loc assign that to the users trips list
 class UserViewModel: ObservableObject {
     @Published var user: User?
     @Published var current_trip: Trip?
+    
+    func createTrip(start: POI, end: POI){
+        current_trip = Trip(start_location: start, end_location: end)
+    }
+        
+    func addTripToUser(trip: Trip){
+        if var user = user {
+            user.addTrip(trip: trip)
+        }
+        
+    }
 }
 
