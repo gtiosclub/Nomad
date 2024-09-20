b0f43efd59c8a7fed405749ea21266054c49df2f
nac5504 <nac5504@gmail.com>
2024-09-19 21:10:21 -0400

    fixed plist issue

--- Nomad/Views/MapView.swift
+++ Nomad/Views/MapView.swift
@@ -4,14 +4,42 @@
 //
 //  Created by Nicholas Candello on 9/15/24.
 //
-
 import SwiftUI
+import MapKit
 
+@available(iOS 17.0, *)
 struct MapView: View {
+    @ObservedObject var mapManager = MapManager()
+    
+    private let startingPoint = CLLocationCoordinate2D(
+        latitude: 40.83657722488077,
+        longitude: 14.306896671048852
+    )
+    
+    private let destinationCoordinates = CLLocationCoordinate2D(
+        latitude: 40.849761,
+        longitude: 14.263364
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map {
+            // Adding the marker for the starting point
+            Marker("Start", coordinate: mapManager.source.coordinate)
+            Marker("Finish", coordinate: mapManager.destination.coordinate)
+            
+            // Show the route if it is available
+            if let route = mapManager.route {
+                MapPolyline(route)
+                    .stroke(.blue, lineWidth: 5)
+            }
+        }.onAppear() {
+            mapManager.setSource(coord: startingPoint)
+            mapManager.setDestination(coord: destinationCoordinates)
+            mapManager.getDirections()
+        }
     }
 }
+
 
 #Preview {
     MapView()
