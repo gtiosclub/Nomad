415621929eba9962eab8417292390b7bb71e5295
jifonthespoon <austin.huguenard@gmail.com>
2024-09-19 20:11:02 -0400

    user init

--- Nomad/ViewModels/UserViewModel.swift
+++ Nomad/ViewModels/UserViewModel.swift
@@ -11,5 +11,9 @@ import MapKit
 class UserViewModel: ObservableObject {
     @Published var user: User?
     @Published var current_trip: Trip?
+    
+    init(user: User) {
+        self.user = user
+    }
 }
 
