548eaa6652cccb046a2066d781858ea48c43a93e
Amber Verma <ambersverma@gmail.com>
2024-09-24 15:56:26 -0400

    Added setStartDate and setEndDate for userViewModel

--- Nomad/ViewModels/UserViewModel.swift
+++ Nomad/ViewModels/UserViewModel.swift
@@ -11,5 +11,28 @@ import MapKit
 class UserViewModel: ObservableObject {
     @Published var user: User?
     @Published var current_trip: Trip?
-}
+    var start_date: String?
+    var end_date: String?
+    
+    func setStartDate(startDate: String) {
+            if var user = self.user, var trip = self.current_trip {
+                trip.start_date = startDate
+                
+                if let tripIndex = user.trips.firstIndex(where: { $0.id == trip.id }) {
+                    user.trips[tripIndex] = trip
+                    self.user = user
+                }
+            }
+        }
 
+        func setEndDate(endDate: String) {
+            if var user = self.user, var trip = self.current_trip {
+                trip.end_date = endDate
+                
+                if let tripIndex = user.trips.firstIndex(where: { $0.id == trip.id }) {
+                    user.trips[tripIndex] = trip
+                    self.user = user
+                }
+            }
+        }
+    }
