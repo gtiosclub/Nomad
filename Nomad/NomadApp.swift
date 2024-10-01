73791567f7c4dcf318fb790cd18ca81c9c14064b
DattaKansal <dtkansal@gmail.com>
2024-10-01 18:29:17 -0400

    updated firebase stuff

--- Nomad/NomadApp.swift
+++ Nomad/NomadApp.swift
@@ -24,7 +24,7 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 
 @main
 struct YourApp: App {
-  // register app delegate for Firebase setup
+
   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
   var body: some Scene {
