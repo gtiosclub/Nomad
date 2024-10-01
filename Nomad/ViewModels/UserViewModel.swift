899002c54dab20785d58b929953ae3c45e37e2a6
Neel <neel468167@gmail.com>
2024-10-01 03:22:00 -0400

    Issue #65

--- Nomad/ViewModels/UserViewModel.swift
+++ Nomad/ViewModels/UserViewModel.swift
@@ -7,10 +7,13 @@
 
 import Foundation
 import MapKit
+import CoreLocation
 
 class UserViewModel: ObservableObject {
     @Published var user: User?
     @Published var current_trip: Trip?
+    @Published var total_distance: Double = 0
+    @Published var total_time: Double = 0
     
     init(user: User? = nil) {
         self.user = user
@@ -54,6 +57,10 @@ class UserViewModel: ObservableObject {
     
     func setCurrentTrip(trip: Trip) {
         self.current_trip = trip
+        Task{
+            await getTotalDistance()
+            await getTotalTime()
+        }
     }
     
     func setStartLocation(new_start_location: POI) {
@@ -86,6 +93,121 @@ class UserViewModel: ObservableObject {
         if let trip = user.findTrip(id: tripID) {
             current_trip = trip
         }
+        
+        Task{
+            await getTotalDistance()
+            await getTotalTime()
+        }
     }
+    
+    func getTotalDistance() async {
+        guard let current_trip else { return }
+        
+        var totalDist = 0.0
+        let stops = current_trip.getStops()
+        if stops.count == 0 {
+            totalDist = await getDistance(fromAddress: current_trip.getStartLocation().address, toAddress: current_trip.getEndLocation().address)
+        } else {
+            totalDist += await getDistance(fromAddress: current_trip.getStartLocation().address, toAddress: stops[0].address)
+            for i in 1..<stops.count {
+                totalDist += await getDistance(fromAddress: stops[i-1].address, toAddress: stops[i].address)
+            }
+            totalDist += await getDistance(fromAddress: stops[stops.count-1].address, toAddress: current_trip.getEndLocation().address)
+        }
+        total_distance = totalDist
+    }
+    
+    func getTotalTime() async {
+        guard let current_trip else { return }
+        
+        var totalTime = 0.0
+        let stops = current_trip.getStops()
+        if stops.count == 0 {
+            totalTime = await getTime(fromAddress: current_trip.getStartLocation().address, toAddress: current_trip.getEndLocation().address)
+        } else {
+            totalTime += await getTime(fromAddress: current_trip.getStartLocation().address, toAddress: stops[0].address)
+            for i in 1..<stops.count {
+                totalTime += await getTime(fromAddress: stops[i-1].address, toAddress: stops[i].address)
+            }
+            totalTime += await getTime(fromAddress: stops[stops.count-1].address, toAddress: current_trip.getEndLocation().address)
+        }
+        total_time = totalTime
+    }
+
+    func getDistance(fromAddress: String, toAddress: String) async -> (Double) {
+        let geoCoder = CLGeocoder()
+        var fromLocation: CLLocation?
+        var toLocation: CLLocation?
+        
+        do {
+            if let fromPlacemark = try await geoCoder.geocodeAddressString(fromAddress).first,
+               let toPlacemark = try await geoCoder.geocodeAddressString(toAddress).first {
+                fromLocation = fromPlacemark.location
+                toLocation = toPlacemark.location
+            }
+        } catch {
+            print("Error during geocoding: \(error)")
+            return 0.0
+        }
+        
+        guard let fromLocation, let toLocation else { return 0.0 }
+        
+        let fromPlacemark = MKPlacemark(coordinate: fromLocation.coordinate)
+        let toPlacemark = MKPlacemark(coordinate: toLocation.coordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: fromPlacemark)
+        request.destination = MKMapItem(placemark: toPlacemark)
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        do {
+            let response = try await directions.calculate()
+            return response.routes.first?.distance ?? 0.0
+        } catch {
+            print("Error: \(error)")
+        }
+        
+        return 0.0
+    }
+    
+    func getTime(fromAddress: String, toAddress: String) async -> (Double) {
+        let geoCoder = CLGeocoder()
+        var fromLocation: CLLocation?
+        var toLocation: CLLocation?
+        
+        do {
+            if let fromPlacemark = try await geoCoder.geocodeAddressString(fromAddress).first,
+               let toPlacemark = try await geoCoder.geocodeAddressString(toAddress).first {
+                fromLocation = fromPlacemark.location
+                toLocation = toPlacemark.location
+            }
+        } catch {
+            print("Error during geocoding: \(error)")
+            return 0.0
+        }
+        
+        guard let fromLocation, let toLocation else { return 0.0 }
+        
+        let fromPlacemark = MKPlacemark(coordinate: fromLocation.coordinate)
+        let toPlacemark = MKPlacemark(coordinate: toLocation.coordinate)
+        
+        let request = MKDirections.Request()
+        request.source = MKMapItem(placemark: fromPlacemark)
+        request.destination = MKMapItem(placemark: toPlacemark)
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        do {
+            let response = try await directions.calculate()
+            return response.routes.first?.expectedTravelTime ?? 0.0
+        } catch {
+            print("Error: \(error)")
+        }
+        
+        return 0.0
+    }
+
+
 }
     
