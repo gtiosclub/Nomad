8931ad280db9c7b97d2cfbac4bc3fcaad1988e26
shaunakkarnik <shaunak.karnik.14@gmail.com>
2024-10-06 17:22:40 -0400

    added basic ui for recap and detailrecapview

--- Nomad/Views/DetailRecapView.swift
+++ Nomad/Views/DetailRecapView.swift
@@ -2,17 +2,108 @@
 //  DetailRecapView.swift
 //  Nomad
 //
-//  Created by Shaunak Karnik on 9/19/24.
+//  Created by Shaunak Karnik on 10/3/24.
 //
 
 import SwiftUI
+import PhotosUI
 
 struct DetailRecapView: View {
+    let title: String
+    
+    @State private var selectedDay = 1
+    @State var selectedItems: [PhotosPickerItem] = []
+    @State var recapImages: [Image] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                HStack {
+                    VStack (alignment: .leading){
+                        Text(title)
+                            .font(.system(size: 18, weight: .semibold))
+                            .padding(.bottom, 15)
+                        PhotosPicker(selection: $selectedItems,
+                                     matching: .any(of: [.images, .not(.screenshots)])) {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .strokeBorder(style: StrokeStyle(lineWidth: 1, dash: [2]))
+                                    .frame(width: 200, height: 150)
+                                Image(systemName: "plus.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 30)
+                            }.padding(.bottom, 30)
+                        }
+                        Text("Trip Details")
+                            .font(.system(size: 18, weight: .semibold))
+                            .padding(.bottom, 10)
+                        HStack {
+                            Image(systemName: "mappin")
+                            Text("Los Angeles, CA")
+                        }.padding(.bottom, 10)
+                    }
+                    Spacer()
+                }
+                HStack {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.gray.opacity(0.5))
+                            .frame(width: 155, height: 87)
+                        VStack {
+                            Text("428")
+                                .font(.system(size: 30))
+                            Text("miles traveled")
+                        }
+                    }
+                    Spacer()
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.gray.opacity(0.5))
+                            .frame(width: 155, height: 87)
+                        VStack {
+                            Text("51")
+                                .font(.system(size: 30))
+                            Text("hours spent")
+                        }
+                    }
+                }.padding(.bottom, 40)
+                HStack {
+                    Text("Here are the places you stopped:")
+                        .font(.system(size: 18, weight: .semibold))
+                        .padding(.bottom, 10)
+                    Spacer()
+                }
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(.gray, lineWidth: 1)
+                    .frame(width: .infinity, height: 400)
+                    .padding(.bottom, 20)
+                HStack {
+                    Text("Visual road maps:")
+                        .font(.system(size: 18, weight: .semibold))
+                    Spacer()
+                }
+                Picker("Select a day", selection: $selectedDay) {
+                    Text("Day 1").tag(1)
+                    Text("Day 2").tag(2)
+                    Text("Day 3").tag(3)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                .padding(.bottom, 10)
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.gray, lineWidth: 1)
+                        .frame(width: .infinity, height: 300)
+                    Text("Day \(selectedDay)")
+                }
+                
+                
+            }.padding(.horizontal, 30)
+                .padding(.bottom, 30)
+        }
     }
 }
 
 #Preview {
-    DetailRecapView()
+    DetailRecapView(title: "California")
 }
--- Nomad/Views/RecapView.swift
+++ Nomad/Views/RecapView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct RecapView: View {
+    var cards: [CardView] = [CardView(title: "Scenic California Mountain Route", attributes: ["4-6 Hours", "Pet-friendly"]), CardView(title: "Johnson Family Spring Retreat", attributes: ["0-1 Hours", "Scenic"]), CardView(title: "Scenic California Mountain Route", attributes: ["4-6 Hours", "Pet-friendly"])]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Let's see where you've been!")
+                    .font(.system(size: 18, weight: .semibold))
+                    .padding(.bottom, 20)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                let columns = [GridItem(.flexible(), spacing: 20), GridItem(.flexible(), spacing: 10)]
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(cards) { card in
+                        NavigationLink {
+                            DetailRecapView(title: card.title)
+                        } label: {
+                            card
+                        }.buttonStyle(PlainButtonStyle())
+                    }
+                }
+                Spacer()
+            }.padding(20)
+        }
+    }
+}
+
+struct CardView: View, Identifiable {
+    let id = UUID()
+    let title: String
+    let attributes: [String]
+
+    var body: some View {
+        VStack {
+            Rectangle()
+                .fill(Color.gray.opacity(0.5))
+                .frame(width: 165, height: 135)
+                .cornerRadius(10)
+
+            Text(title)
+                .font(.subheadline)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 5)
+
+            let columns = [GridItem(.flexible()), GridItem(.flexible())]
+            LazyVGrid(columns: columns, spacing: 8) {
+                ForEach(attributes, id: \.self) { attribute in
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray.opacity(0.5))
+                        .frame(height: 20)
+                        .overlay(Text(attribute)
+                                    .font(.caption2))
+                }
+            }
+        }
     }
 }
 
 #Preview {
     RecapView()
 }
+
