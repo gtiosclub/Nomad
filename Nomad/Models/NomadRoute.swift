8522ad1b2fa70a29a0ed4e947fc7319c9904cb24
nac5504 <nac5504@gmail.com>
2024-10-10 16:55:36 -0400

    changed route to have legs and steps within

--- Nomad/Models/NomadRoute.swift
+++ Nomad/Models/NomadRoute.swift
@@ -11,7 +11,7 @@ import MapboxDirections
 struct NomadRoute {
     let id = UUID()
     var route: Route?
-    var steps: [Step]
+    var legs: [NomadLeg]
     
     // returns an array of polylines for each step of the route.
     func getRoutePolyline() -> MKPolyline {
@@ -25,12 +25,50 @@ struct NomadRoute {
         return polyline
     }
     
-    func getStartLocation() -> CLLocationCoordinate2D? {
-        return steps[0].startCoordinate
+//    func getStartLocation() -> CLLocationCoordinate2D? {
+//        return steps[0].startCoordinate
+//    }
+//    
+//    func getEndLocation() -> CLLocationCoordinate2D? {
+//        steps[steps.count - 1].endCoordinate
+//    }
+    static func convertToMKPolyline(_ coords: [LocationCoordinate2D]) -> MKPolyline {
+        var coordinates = [CLLocationCoordinate2D]()
+        for coord in coords {
+            coordinates.append(CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude))
+        }
+        return MKPolyline(coordinates: coordinates, count: coordinates.count)
+    }
+}
+
+struct NomadLeg {
+    let id = UUID()
+    var steps: [Step]
+    var startCoordinate: CLLocationCoordinate2D
+    var endCoordinate: CLLocationCoordinate2D
+    var routeShape: MKPolyline
+    private var coords: [CLLocationCoordinate2D]
+    
+    init(leg: MapboxDirections.RouteLeg) {
+        
     }
     
-    func getEndLocation() -> CLLocationCoordinate2D? {
-        steps[steps.count - 1].endCoordinate
+    init(steps: [NomadStep]) {
+        
+    }
+    func getStartLocation() -> CLLocationCoordinate2D {
+        return startCoordinate
+    }
+    func getEndLocation() -> CLLocationCoordinate2D {
+        return endCoordinate
+    }
+    
+    func getShape() -> MKPolyline {
+        return routeShape
+    }
+    
+    func getCoordinates() -> [CLLocationCoordinate2D] {
+        return coords
     }
 }
 
@@ -54,12 +92,14 @@ struct Step {
     var endCoordinate: CLLocationCoordinate2D?
     var direction: Direction
     var exit: Exit?
+    private var coords: [CLLocationCoordinate2D]
     
     
     init(step: RouteStep) {
         self.startCoordinate = step.shape?.coordinates.first
         self.endCoordinate = step.shape?.coordinates.last
-        self.routeShape = Step.convertToMKPolyline(step.shape?.coordinates ?? [])
+        self.coords = step.shape?.coordinates ?? []
+        self.routeShape = NomadRoute.convertToMKPolyline(step.shape?.coordinates ?? [])
         self.direction = Direction(distance: step.distance, instructions: step.instructions, expectedTravelTime: step.expectedTravelTime)
         if step.destinations != nil || step.exitCodes != nil || step.exitNames != nil {
             self.exit = Exit(destinations: step.destinations, exitCodes: step.exitCodes, exitNames: step.exitNames)
@@ -68,19 +108,28 @@ struct Step {
         }
     }
     
+    // placeholder data
     init() {
         self.startCoordinate = CLLocationCoordinate2D(latitude: 33.7501, longitude: 84.3885)
         self.endCoordinate = CLLocationCoordinate2D(latitude: 32.7501, longitude: 83.3885)
-        self.routeShape = Step.convertToMKPolyline([startCoordinate!, endCoordinate!])
+        self.routeShape = NomadRoute.convertToMKPolyline([startCoordinate!, endCoordinate!])
         self.direction = Direction(distance: 100, instructions: "Turn right in 100 miles", expectedTravelTime: TimeInterval(5000))
         self.exit = Exit(destinations: ["Atlanta", "New York"], exitCodes: ["78", "79"], exitNames: ["Georgia Ave.","Peachtree St."])
     }
     
-    static func convertToMKPolyline(_ coords: [LocationCoordinate2D]) -> MKPolyline {
-        var coordinates = [CLLocationCoordinate2D]()
-        for coord in coords {
-            coordinates.append(CLLocationCoordinate2D(latitude: coord.latitude, longitude: coord.longitude))
-        }
-        return MKPolyline(coordinates: coordinates, count: coordinates.count)
+    
+    func getStartLocation() -> CLLocationCoordinate2D {
+        return startCoordinate ?? CLLocationCoordinate2D()
+    }
+    func getEndLocation() -> CLLocationCoordinate2D {
+        return endCoordinate ?? CLLocationCoordinate2D()
+    }
+    
+    func getShape() -> MKPolyline {
+        return routeShape
+    }
+    
+    func getCoordinates() -> [CLLocationCoordinate2D] {
+        return coords
     }
 }
