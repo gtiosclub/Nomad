264f577d69d73d32cf75f62be97f4e619ec86e6c
Connor-1233 <Connor.e.case@gmail.com>
2024-10-17 18:51:32 -0400

    Adding UI File

---
+++ Nomad/Views/POIDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  POIDetailView.swift
+//  Nomad
+//
+//  Created by Connor on 10/17/24.
+//
+
+import SwiftUI
+
+struct POIDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    POIDetailView()
+}
