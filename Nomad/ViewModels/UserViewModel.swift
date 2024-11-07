d1b227cf8a678507a0fce421448587e1c2dfed8c
jifonthespoon <austin.huguenard@gmail.com>
2024-11-07 18:15:01 -0500

    Force trip route to be created before navigating to find stop view

--- Nomad/ViewModels/UserViewModel.swift
+++ Nomad/ViewModels/UserViewModel.swift
@@ -58,10 +58,12 @@ class UserViewModel: ObservableObject {
     
     @MainActor
     func createTrip(start_location: any POI, end_location: any POI, start_date: String = "", end_date: String = "", stops: [any POI] = [], start_time: String = "8:00 AM") async {
-        let route = await getRoute()
+        
 //        let cityImageURL = await Trip.getCityImageAsync(location: end_location)
 //        print(cityImageURL)
-        self.current_trip = Trip(route: route, start_location: start_location, end_location: end_location, start_date: start_date, end_date: end_date, stops: stops, start_time: start_time)
+        self.current_trip = Trip(start_location: start_location, end_location: end_location, start_date: start_date, end_date: end_date, stops: stops, start_time: start_time)
+        let route = await getRoute()
+        self.current_trip?.route = route
         
         self.user.addTrip(trip: self.current_trip!)
     }
