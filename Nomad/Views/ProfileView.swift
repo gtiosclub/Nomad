bc74facff6e294ac89d8f0a02a2e3bcc8061d6c8
DattaKansal <dtkansal@gmail.com>
2024-11-12 14:22:49 -0500

    to do: add profile view

---
+++ Nomad/Views/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Nomad
+//
+//  Created by Datta Kansal on 11/10/24.
+//
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+
+#Preview {
+    ProfileView()
+}
