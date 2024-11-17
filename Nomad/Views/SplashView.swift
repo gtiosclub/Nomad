57bc07a0c69558de1f94674d911243bf982c8baa
royankit11 <royankit11@gmail.com>
2024-11-17 17:30:06 -0500

    Update app icon

--- Nomad/Views/SplashView.swift
+++ Nomad/Views/SplashView.swift
@@ -22,7 +22,7 @@ struct SplashView: View {
             }
         } else {
             VStack {
-                Image("SplashIcon")
+                Image("AtlasIcon")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 150, height: 150)
