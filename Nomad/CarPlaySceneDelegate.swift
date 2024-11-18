18e0b27fc809aae8a8b633d18054acfe85f5e369
shaunakkarnik <shaunak.karnik.14@gmail.com>
2024-11-17 23:09:26 -0500

    carplay prototype done

--- Nomad/CarPlaySceneDelegate.swift
+++ Nomad/CarPlaySceneDelegate.swift
@@ -50,58 +50,133 @@ class CarPlaySceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
 }
 
 // MARK: - Simple CarPlay MapView
+struct CarPlayMapContentView: MapContent {
+    var mapMarkers: [MapMarker] // Replace with your marker type
+    var mapPolylines: [MKPolyline]
+    
+    var body: some MapContent {
+        UserAnnotation()
+        
+        ForEach(mapMarkers) { marker in
+            if marker.icon == .trafficLight || marker.icon == .stopSign {
+                Annotation("", coordinate: marker.coordinate) {
+                    Image(marker.icon.image_path)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30, height: 30)
+                        .clipShape(Circle())
+                }
+            } else {
+                Marker(marker.title, coordinate: marker.coordinate)
+            }
+        }
+        
+        ForEach(mapPolylines, id: \.self) { polyline in
+            MapPolyline(polyline)
+                .stroke(.blue, lineWidth: 5)
+        }
+    }
+}
+
+// MARK: - CarPlay Map View
 struct CarPlayMapView: View {
     @ObservedObject var vm: UserViewModel
     @StateObject private var navManager = NavigationManager.nav
     @StateObject private var mapManager = MapManager.manager
     
     var body: some View {
-        Map(position: $navManager.mapPosition) {
-            // Show user location
-            UserAnnotation()
-            
-            // Show markers
-            ForEach(navManager.mapMarkers) { marker in
-                if marker.icon == .trafficLight || marker.icon == .stopSign {
-                    Annotation("", coordinate: marker.coordinate) {
-                        Image(marker.icon.image_path)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 30, height: 30)
-                            .clipShape(Circle())
-                    }
-                } else {
-                    Marker(marker.title, coordinate: marker.coordinate)
-                }
+        ZStack {
+            // Map View
+            Map(position: $navManager.mapPosition) {
+                CarPlayMapContentView(
+                    mapMarkers: navManager.mapMarkers,
+                    mapPolylines: navManager.mapPolylines
+                )
             }
+            .mapControlVisibility(.hidden)
             
-            // Show route polylines
-            ForEach(navManager.mapPolylines, id: \.self) { polyline in
-                MapPolyline(polyline)
-                    .stroke(.blue, lineWidth: 5)
+            // Navigation Overlay
+            VStack {
+                HStack {
+                    CarPlayTopView()
+                        .frame(width: 175, height: 40)
+                    Spacer()
+                }
+                .padding(10)
+                .padding(.leading, 45)
+                Spacer()
+                HStack {
+                    CarPlayBottomNavView(navManager: navManager)
+                        .frame(width: 175, height: 50)
+                        .padding(.horizontal)
+                        .padding(.bottom, 20)
+                    Spacer()
+                    Image(systemName: "mic")
+                        .padding(.trailing, 15)
+                }
+                .padding(.horizontal, 10)
+                .padding(.leading, 30)
             }
         }
-        .mapControlVisibility(.hidden)
-        .onChange(of: mapManager.motion) { _, newMotion in
-            if let newLocation = newMotion.coordinate {
-                if !navManager.destinationReached {
-                    Task {
-                        await navManager.recalibrateCurrentStep()
-                    }
-                    navManager.distanceToNextManeuver = navManager.assignDistanceToNextManeuver()
+    }
+}
+
+struct CarPlayTopView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+            HStack {
+                VStack {
+                    Image(systemName: "arrow.turn.up.right")
+                        .foregroundColor(.black)
+                    Text("100 ft")
+                        .font(.system(size: 10))
+                        .foregroundColor(.black)
                 }
-                
-                if let camera = navManager.mapPosition.camera,
-                   !navManager.movingMap(camera: camera.centerCoordinate) {
-                    withAnimation {
-                        navManager.updateMapPosition(newMotion)
-                    }
-                }
+                Text("Turn Right")
+                    .font(.system(size: 24))
+                    .foregroundColor(.black)
             }
         }
-        .onAppear {
-            let motion = mapManager.motion
-            navManager.updateMapPosition(motion)
+    }
+}
+
+struct CarPlayBottomNavView: View {
+    // Change to ObservedObject since we're passing it in
+    @ObservedObject var navManager: NavigationManager
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10.0)
+            HStack {
+                VStack {
+                    Text("7:32")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(.black)
+                    Text("arrival")
+                        .font(.system(size: 14))
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                VStack {
+                    Text("2:26")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(.black)
+                    Text("hrs")
+                        .font(.system(size: 14))
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                VStack {
+                    Text("121")
+                        .font(.system(size: 18, weight: .bold))
+                        .foregroundColor(.black)
+                    Text("mi")
+                        .font(.system(size: 14))
+                        .foregroundColor(.black)
+                }
+            }
+            .padding(.horizontal, 10)
         }
     }
 }
